b841644e16e2b48322e3cf881244ab40cd4d48c7
Satyam Maurya <mauryasatyam066@gmail.com>
2025-04-24 14:16:26 +0530

    Todo_using_Axios

--- TodoList_1/tODOlIST_1/src/components/Todo_Api.jsx
+++ TodoList_1/tODOlIST_1/src/components/Todo_Api.jsx
@@ -1,3 +1,6 @@
+// fetch Api
+/*
+
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = async () => {
@@ -50,4 +53,43 @@ const deleteTodo = async (id) => {
     }
   };
   */
+ // Axios
+ import axios from 'axios';
+
+ const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+// Get todos (limit 7)
+const getTodos = async () => {
+  const response = await axios.get(API_URL, {
+    params: {
+      _limit: 7
+    }
+  });
+  return response.data;
+};
+
+
+const addTodo = async (title) => {
+  const response = await axios.post(API_URL, {
+    title,
+    completed: false,
+    userId: 1
+  });
+  return response.data;
+};
+
+
+const deleteTodo = async (id) => {
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.status === 200;
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    return false;
+  }
+};
+
+
 export { getTodos, addTodo, deleteTodo };
+
+
